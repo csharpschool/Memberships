aa108396a060b0e661609b60203cb19e5d0b9be9
Jonas <N/A>
2017-03-15 18:17:13 +0100

    Add project files.

---
+++ Memberships/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Memberships.Startup))]
+namespace Memberships
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
