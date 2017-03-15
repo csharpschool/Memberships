da685a63a94e1a76e9ba8cbfe678e7068a2b5939
Jonas <N/A>
2017-03-15 19:42:41 +0100

    Added the Controller and Views for the Part table

---
+++ Memberships/Areas/Admin/Controllers/PartController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Memberships.Entities;
+using Memberships.Models;
+
+namespace Memberships.Areas.Admin.Controllers
+{
+    //[Authorize(Roles = "Admin")]
+    public class PartController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Part
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Parts.ToListAsync());
+        }
+
+        // GET: Admin/Part/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Part part = await db.Parts.FindAsync(id);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            return View(part);
+        }
+
+        // GET: Admin/Part/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Part/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Title")] Part part)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Parts.Add(part);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(part);
+        }
+
+        // GET: Admin/Part/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Part part = await db.Parts.FindAsync(id);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            return View(part);
+        }
+
+        // POST: Admin/Part/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Title")] Part part)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(part).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(part);
+        }
+
+        // GET: Admin/Part/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Part part = await db.Parts.FindAsync(id);
+            if (part == null)
+            {
+                return HttpNotFound();
+            }
+            return View(part);
+        }
+
+        // POST: Admin/Part/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Part part = await db.Parts.FindAsync(id);
+            var isUnused = await db.Items.CountAsync(i => i.PartId.Equals(id)) == 0;
+            if (isUnused)
+            {
+                db.Parts.Remove(part);
+                await db.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
