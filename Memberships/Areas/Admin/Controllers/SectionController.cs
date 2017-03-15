6a7479c578bdbe3c2d682e259d5c7095b9265765
Jonas <N/A>
2017-03-15 19:38:14 +0100

    Added Controller and Views for the Section table. Many of the partial views used during the course has been added here.

---
+++ Memberships/Areas/Admin/Controllers/SectionController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Memberships.Entities;
+using Memberships.Models;
+
+namespace Memberships.Areas.Admin.Controllers
+{
+    //[Authorize(Roles = "Admin")]
+    public class SectionController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Section
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Sections.ToListAsync());
+        }
+
+        // GET: Admin/Section/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Section section = await db.Sections.FindAsync(id);
+            if (section == null)
+            {
+                return HttpNotFound();
+            }
+            return View(section);
+        }
+
+        // GET: Admin/Section/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Section/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(
+            [Bind(Include = "Id,Title")] Section section)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sections.Add(section);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(section);
+        }
+
+        // GET: Admin/Section/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Section section = await db.Sections.FindAsync(id);
+            if (section == null)
+            {
+                return HttpNotFound();
+            }
+            return View(section);
+        }
+
+        // POST: Admin/Section/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(
+            [Bind(Include = "Id,Title")] Section section)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(section).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(section);
+        }
+
+        // GET: Admin/Section/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Section section = await db.Sections.FindAsync(id);
+            if (section == null)
+            {
+                return HttpNotFound();
+            }
+            return View(section);
+        }
+
+        // POST: Admin/Section/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Section section = await db.Sections.FindAsync(id);
+            var isUnused = await db.Items.CountAsync(i => i.SectionId.Equals(id)) == 0;
+            if (isUnused)
+            {
+                db.Sections.Remove(section);
+                await db.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Memberships/Areas/Admin/Models/SmallButtonModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace Memberships.Areas.Admin.Models
+{
+    public class SmallButtonModel
+    {
+        public string Action { get; set; }
+        public string Text { get; set; }
+        public string Glyph { get; set; }
+        public string ButtonType { get; set; }
+        public int? Id { get; set; }
+        public int? ItemId { get; set; }
+        public int? ProductId { get; set; }
+        public int? SubscriptionId { get; set; }
+        public string UserId { get; set; }
+
+        public string ActionParameters
+        {
+            get
+            {
+                var param = new StringBuilder("?");
+                if (Id != null && Id > 0)
+                    param.Append(String.Format("{0}={1}&", "id", Id));
+
+                if (ItemId != null && ItemId > 0)
+                    param.Append(String.Format("{0}={1}&", "itemId", ItemId));
+
+                if (ProductId != null && ProductId > 0)
+                    param.Append(String.Format("{0}={1}&", "productId", ProductId));
+
+                if (SubscriptionId != null && SubscriptionId > 0)
+                    param.Append(String.Format("{0}={1}&", "subscriptionId", SubscriptionId));
+
+                if (UserId != null && !UserId.Equals(string.Empty))
+                    param.Append(string.Format("{0}={1}&", "userId", UserId));
+
+                return param.ToString().Substring(0, param.Length - 1);
+            }
+        }
+    }
+
+}
