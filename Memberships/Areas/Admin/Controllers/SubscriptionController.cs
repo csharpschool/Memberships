8942445474b9a8fc9ddc3323aa0de5087e2b03f1
Jonas <N/A>
2017-03-15 20:19:22 +0100

    Added the Controller and Views for the Subscription table

---
+++ Memberships/Areas/Admin/Controllers/SubscriptionController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Memberships.Entities;
+using Memberships.Models;
+using System.Transactions;
+
+namespace Memberships.Areas.Admin.Controllers
+{
+    //[Authorize(Roles = "Admin")]
+    public class SubscriptionController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/Subscription
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Subscriptions.ToListAsync());
+        }
+
+        // GET: Admin/Subscription/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Subscription subscription = await db.Subscriptions.FindAsync(id);
+            if (subscription == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subscription);
+        }
+
+        // GET: Admin/Subscription/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Subscription/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Title,Description,RegistrationCode")] Subscription subscription)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Subscriptions.Add(subscription);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(subscription);
+        }
+
+        // GET: Admin/Subscription/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Subscription subscription = await db.Subscriptions.FindAsync(id);
+            if (subscription == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subscription);
+        }
+
+        // POST: Admin/Subscription/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Title,Description,RegistrationCode")] Subscription subscription)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(subscription).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(subscription);
+        }
+
+        // GET: Admin/Subscription/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Subscription subscription = await db.Subscriptions.FindAsync(id);
+            if (subscription == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subscription);
+        }
+
+        // POST: Admin/Subscription/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Subscription subscription = await db.Subscriptions.FindAsync(id);
+            using (var transaction = new TransactionScope(
+                    TransactionScopeAsyncFlowOption.Enabled))
+            {
+                try
+                {
+                    var prodSubscr = db.SubscriptionProducts.Where(
+                        sp => sp.SubscriptionId.Equals(id));
+                    db.SubscriptionProducts.RemoveRange(prodSubscr);
+                    db.Subscriptions.Remove(subscription);
+
+                    await db.SaveChangesAsync();
+                    transaction.Complete();
+                }
+                catch { transaction.Dispose(); }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
