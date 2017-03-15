11e779b6e9165019408da823acadcce752b92225
Jonas <N/A>
2017-03-15 19:47:46 +0100

    Added the Controller and Views for the ItemType table

---
+++ Memberships/Areas/Admin/Controllers/ItemTypeController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Memberships.Entities;
+using Memberships.Models;
+
+namespace Memberships.Areas.Admin.Controllers
+{
+    //[Authorize(Roles = "Admin")]
+    public class ItemTypeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Admin/ItemType
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.ItemTypes.ToListAsync());
+        }
+
+        // GET: Admin/ItemType/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemType itemType = await db.ItemTypes.FindAsync(id);
+            if (itemType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemType);
+        }
+
+        // GET: Admin/ItemType/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/ItemType/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Id,Title")] ItemType itemType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ItemTypes.Add(itemType);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(itemType);
+        }
+
+        // GET: Admin/ItemType/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemType itemType = await db.ItemTypes.FindAsync(id);
+            if (itemType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemType);
+        }
+
+        // POST: Admin/ItemType/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "Id,Title")] ItemType itemType)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(itemType).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(itemType);
+        }
+
+        // GET: Admin/ItemType/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemType itemType = await db.ItemTypes.FindAsync(id);
+            if (itemType == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemType);
+        }
+
+        // POST: Admin/ItemType/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            ItemType itemType = await db.ItemTypes.FindAsync(id);
+            var isUnused = await db.Items.CountAsync(i => i.ItemTypeId.Equals(id)) == 0;
+            if (isUnused)
+            {
+                db.ItemTypes.Remove(itemType);
+                await db.SaveChangesAsync();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
