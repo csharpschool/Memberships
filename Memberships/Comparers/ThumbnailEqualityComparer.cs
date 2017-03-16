ba3e9922c58e1f84980641dbe98aff33e608e9f9
Jonas <N/A>
2017-03-16 08:58:51 +0100

    Added the ThumbnailEqualityComparer class

---
+++ Memberships/Comparers/ThumbnailEqualityComparer.cs
@@ -0,0 +1,21 @@
+﻿using Memberships.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.Comparers
+{
+    public class ThumbnailEqualityComparer : IEqualityComparer<ThumbnailModel>
+    {
+        public bool Equals(ThumbnailModel thumb1, ThumbnailModel thumb2)
+        {
+            return thumb1.ProductId.Equals(thumb2.ProductId);
+        }
+
+        public int GetHashCode(ThumbnailModel thumb)
+        {
+            return thumb.ProductId;
+        }
+    }
+}
