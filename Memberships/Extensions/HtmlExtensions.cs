83fbce2a0204a061dacb487b5a526d4df03b7bb9
Jonas <N/A>
2017-03-16 11:04:08 +0100

    Added the GlyphLink method to the HtmlExtensions class

---
+++ Memberships/Extensions/HtmlExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace Memberships.Extensions
+{
+    public static class HtmlExtensions
+    {
+        public static MvcHtmlString GlyphLink(this HtmlHelper htmlHelper,
+            string controller, string action, string text,
+            string glyphicon, string cssClasses = "", string id = "",
+            Dictionary<string, string> attributes = null)
+        {
+            // declare a span for the glyphicon
+            var glyph = string.Format("<span class='glyphicon glyphicon-{0}'></span>",
+                glyphicon);
+
+            // declare the anchor tag
+            var anchor = new TagBuilder("a");
+            if (controller.Length > 0)
+                anchor.MergeAttribute("href", string.Format("/{0}/{1}/",
+                    controller, action));
+            else
+                anchor.MergeAttribute("href", "#");
+
+            if (attributes != null)
+                foreach (var attribute in attributes)
+                    anchor.MergeAttribute(attribute.Key, attribute.Value);
+
+            anchor.InnerHtml = string.Format("{0} {1}", glyph, text);
+            anchor.AddCssClass(cssClasses);
+            anchor.GenerateId(id);
+
+            return MvcHtmlString.Create(anchor.ToString(TagRenderMode.Normal));
+        }
+    }
+}
